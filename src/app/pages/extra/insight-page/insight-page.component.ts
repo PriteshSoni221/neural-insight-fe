47afa5fa64259dc0a032d5218934444306cf7f67
Pritesh University <priteshuniversity@MacBook-Pro-von-leap-2.local>
2025-01-21 18:25:44 +0100

    fix: Normalize line endings

--- src/app/pages/extra/insight-page/insight-page.component.ts
+++ src/app/pages/extra/insight-page/insight-page.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/material.module';
+import { TextSummaryService } from 'src/app/services/text-summary.service';
 
 @Component({
   selector: 'app-insight-page',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule],
   templateUrl: './insight-page.component.html',
   styleUrl: './insight-page.component.scss'
 })
 export class InsightPageComponent {
-  public DEMO_REVIEW: string = "ex. I was a bit unsettled at first after the negative reviews, but the Asus Vivobook 15 Pro OLED equipped with AMD Ryzen 9 7940 HS and Nvidia RTX 4060 as well as 32GB RAM has consistently pleasantly surprised me."
+  public review: FormControl<string | null> = new FormControl('');
+  public summary: string = ""
+
+  constructor(private textSummaryService: TextSummaryService) { }
+
+  public onSummarize(): void {
+    console.log("review", this.review.value);
+
+    if (this.review.value) {
+      this.textSummaryService.getSummary(this.review.value).subscribe({
+        next: (response) => {
+          this.summary = response.summary;
+        },
+        error: (error) => {
+          console.error("error fetching summary", error);
+          this.summary = "An error occurred while fetching the summary."
+        }
+      })
+    } else {
+      alert("Please enter a review first.")
+    }
+    // if (this.review.value) {
+    //   this.textSummaryService.getSummary(this.review.value).subscribe(response => {
+    //     console.log("response", response)
+    //   })
+    // }
+  }
 }
