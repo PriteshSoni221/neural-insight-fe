f4d9b464ff47b9a15eb1ed75c73bf071e5898a4f
PriteshSoni221 <priteshsoni263@gmail.com>
2025-01-23 18:58:23 +0100

    feat: added bar and pie chart on dashboard

--- src/app/pages/starter/starter.component.ts
+++ src/app/pages/starter/starter.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { MaterialModule } from '../../material.module';
-import { AppProfitExpensesComponent } from 'src/app/components/profit-expenses/profit-expenses.component';
-import { AppTrafficDistributionComponent } from 'src/app/components/traffic-distribution/traffic-distribution.component';
+import { AppProfitExpensesComponent, profitExpanceChart } from 'src/app/components/profit-expenses/profit-expenses.component';
+import { AppTrafficDistributionComponent, trafficdistributionChart } from 'src/app/components/traffic-distribution/traffic-distribution.component';
 import { AppProductSalesComponent } from 'src/app/components/product-sales/product-sales.component';
 import { AppUpcomingSchedulesComponent } from 'src/app/components/upcoming-schedules/upcoming-schedules.component';
 import { AppTopEmployeesComponent } from 'src/app/components/top-employees/top-employees.component';
@@ -24,4 +24,146 @@ import { AppBlogComponent } from 'src/app/components/apps-blog/apps-blog.compone
   templateUrl: './starter.component.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class StarterComponent { }
+
+export class StarterComponent {
+  public trafficdistributionChart!: Partial<trafficdistributionChart> | any;
+  public aspectSentimentChart!: Partial<profitExpanceChart> | any
+
+  constructor(){
+    this.loadDistributionChart()
+    this.loadChart()
+  }
+
+  private loadChart(): void {
+    this.aspectSentimentChart = {
+      series: [
+        {
+          name: 'Number of positive ratings',
+          data: [1, 1, 0, 0, 1],
+          color: '#0085db',
+        },
+        {
+          name: 'Number of negative ratings',
+          data: [0, 1, 1, 1, 1],
+          color: '#fb977d',
+        },
+      ],
+
+      grid: {
+        borderColor: 'rgba(0,0,0,0.1)',
+        strokeDashArray: 3,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '30%',
+          borderRadius: 4,
+          endingShape: "rounded",
+        },
+      },
+      chart: {
+        type: 'bar',
+        height: 390,
+        offsetY: 10,
+        foreColor: '#adb0bb',
+        fontFamily: 'inherit',
+        toolbar: { show: false },
+      },
+      dataLabels: { enabled: false },
+      markers: { size: 0 },
+      legend: { show: true },
+      xaxis: {
+        type: 'category',
+        categories: ['Price', 'Quality', 'Delivery', 'Packaging', 'Service'],
+        axisTicks: {
+          show: false,
+        },
+        axisBorder: {
+          show: false,
+        },
+        labels: {
+          style: { cssClass: 'grey--text lighten-2--text fill-color' },
+        },
+      },
+      stroke: {
+        show: true,
+        width: 5,
+        colors: ['transparent'],
+      },
+      tooltip: { theme: 'light' },
+
+      responsive: [
+        {
+          breakpoint: 600,
+          options: {
+            plotOptions: {
+              bar: {
+                borderRadius: 3,
+              },
+            },
+          },
+        },
+      ],
+    }
+  }
+
+  private loadDistributionChart(): void {
+      this.trafficdistributionChart = {
+        series: [12, 8, 15],
+        labels: ['neutral', 'negative', 'positive'],
+        chart: {
+          type: 'donut',
+          fontFamily: "'Plus Jakarta Sans', sans-serif;",
+          foreColor: '#adb0bb',
+          toolbar: {
+            show: false,
+          },
+          height: 160,
+        },
+        colors: ['#f8c076', '#fb977d', '#0085db'],
+        plotOptions: {
+          pie: {
+            donut: {
+              size: '80%',
+              background: 'none',
+              labels: {
+                show: true,
+                name: {
+                  show: true,
+                  fontSize: '12px',
+                  color: undefined,
+                  offsetY: 5,
+                },
+                value: {
+                  show: true,
+                  color: '#98aab4',
+                },
+              },
+            },
+          },
+        },
+        stroke: {
+          show: false,
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        legend: {
+          show: true,
+        },
+        responsive: [
+          {
+            breakpoint: 491,
+            options: {
+              chart: {
+                width: 120,
+              },
+            },
+          },
+        ],
+        tooltip: {
+          enabled: false,
+        },
+      };
+    }
+ }
