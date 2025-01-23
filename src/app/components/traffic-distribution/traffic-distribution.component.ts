ff622a9ab8019c7d2582db31311bd500a6a1a13a
PriteshSoni221 <priteshsoni263@gmail.com>
2025-01-23 18:25:21 +0100

    feat: aspect based chart logic shifted

--- src/app/components/traffic-distribution/traffic-distribution.component.ts
+++ src/app/components/traffic-distribution/traffic-distribution.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { TablerIconsModule } from 'angular-tabler-icons';
 import { MaterialModule } from 'src/app/material.module';
 import { MatButtonModule } from '@angular/material/button';
@@ -46,68 +46,5 @@ export class AppTrafficDistributionComponent {
 
   @ViewChild('chart') chart: ChartComponent = Object.create(null);
 
-  public trafficdistributionChart!: Partial<trafficdistributionChart> | any;
-
-
-  constructor() {
-
-    this.trafficdistributionChart = {
-      series: [5368, 3500, 4106],
-      labels: ['5368', 'Refferal Traffic', 'Oragnic Traffic'],
-      chart: {
-        type: 'donut',
-        fontFamily: "'Plus Jakarta Sans', sans-serif;",
-        foreColor: '#adb0bb',
-        toolbar: {
-          show: false,
-        },
-        height: 160,
-      },
-      colors: ['#e7ecf0', '#fb977d', '#0085db'],
-      plotOptions: {
-        pie: {
-          donut: {
-            size: '80%',
-            background: 'none',
-            labels: {
-              show: true,
-              name: {
-                show: true,
-                fontSize: '12px',
-                color: undefined,
-                offsetY: 5,
-              },
-              value: {
-                show: false,
-                color: '#98aab4',
-              },
-            },
-          },
-        },
-      },
-      stroke: {
-        show: false,
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: false,
-      },
-      responsive: [
-        {
-          breakpoint: 991,
-          options: {
-            chart: {
-              width: 120,
-            },
-          },
-        },
-      ],
-      tooltip: {
-        enabled: false,
-      },
-    };
-
-  }
+  @Input() trafficdistributionChart!: Partial<trafficdistributionChart> | any;
 }
--- src/app/pages/extra/insight-page/insight-page.component.ts
+++ src/app/pages/extra/insight-page/insight-page.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AppProfitExpensesComponent, profitExpanceChart } from 'src/app/components/profit-expenses/profit-expenses.component';
+import { AppTrafficDistributionComponent, trafficdistributionChart } from 'src/app/components/traffic-distribution/traffic-distribution.component';
 import { MaterialModule } from 'src/app/material.module';
 import { TextSummaryService } from 'src/app/services/text-summary.service';
 
 @Component({
   selector: 'app-insight-page',
   standalone: true,
-  imports: [MaterialModule, ReactiveFormsModule, CommonModule, AppProfitExpensesComponent],
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule, AppProfitExpensesComponent, AppTrafficDistributionComponent],
   templateUrl: './insight-page.component.html',
   styleUrl: './insight-page.component.scss'
 })
@@ -16,6 +17,7 @@ export class InsightPageComponent {
   public review: FormControl<string | null> = new FormControl('');
   public summary: string = ""
   public aspectSentimentChart!: Partial<profitExpanceChart> | any
+  public trafficdistributionChart!: Partial<trafficdistributionChart> | any;
 
   constructor(private textSummaryService: TextSummaryService) { }
 
@@ -26,76 +28,8 @@ export class InsightPageComponent {
       this.textSummaryService.getSummary(this.review.value).subscribe({
         next: (response) => {
           this.summary = response.summary;
-          this.aspectSentimentChart = {
-            series: [
-              {
-                name: 'Eanings this month',
-                data: [9, 5, 3, 7, 5, 10, 3],
-                color: '#0085db',
-              },
-              {
-                name: 'Expense this month',
-                data: [6, 3, 9, 5, 4, 6, 4],
-                color: '#fb977d',
-              },
-            ],
-
-            grid: {
-              borderColor: 'rgba(0,0,0,0.1)',
-              strokeDashArray: 3,
-            },
-            plotOptions: {
-              bar: {
-                horizontal: false,
-                columnWidth: '30%',
-                borderRadius: 4,
-                endingShape: "rounded",
-              },
-            },
-            chart: {
-              type: 'bar',
-              height: 390,
-              offsetY: 10,
-              foreColor: '#adb0bb',
-              fontFamily: 'inherit',
-              toolbar: { show: false },
-            },
-            dataLabels: { enabled: false },
-            markers: { size: 0 },
-            legend: { show: false },
-            xaxis: {
-              type: 'category',
-              categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-              axisTicks: {
-                show: false,
-              },
-              axisBorder: {
-                show: false,
-              },
-              labels: {
-                style: { cssClass: 'grey--text lighten-2--text fill-color' },
-              },
-            },
-            stroke: {
-              show: true,
-              width: 5,
-              colors: ['transparent'],
-            },
-            tooltip: { theme: 'light' },
-
-            responsive: [
-              {
-                breakpoint: 600,
-                options: {
-                  plotOptions: {
-                    bar: {
-                      borderRadius: 3,
-                    },
-                  },
-                },
-              },
-            ],
-          }
+          this.loadChart()
+          this.loadDistributionChart()
         },
         error: (error) => {
           console.error("error fetching summary", error);
@@ -105,5 +39,138 @@ export class InsightPageComponent {
     } else {
       alert("Please enter a review first.")
     }
+  }
+
+  private loadChart(): void {
+    this.aspectSentimentChart = {
+      series: [
+        {
+          name: 'Number of positive ratings',
+          data: [1, 1, 0, 0, 1],
+          color: '#0085db',
+        },
+        {
+          name: 'Number of negative ratings',
+          data: [0, 1, 1, 1, 1],
+          color: '#fb977d',
+        },
+      ],
+
+      grid: {
+        borderColor: 'rgba(0,0,0,0.1)',
+        strokeDashArray: 3,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '30%',
+          borderRadius: 4,
+          endingShape: "rounded",
+        },
+      },
+      chart: {
+        type: 'bar',
+        height: 390,
+        offsetY: 10,
+        foreColor: '#adb0bb',
+        fontFamily: 'inherit',
+        toolbar: { show: false },
+      },
+      dataLabels: { enabled: false },
+      markers: { size: 0 },
+      legend: { show: true },
+      xaxis: {
+        type: 'category',
+        categories: ['Price', 'Quality', 'Delivery', 'Packaging', 'Service'],
+        axisTicks: {
+          show: false,
+        },
+        axisBorder: {
+          show: false,
+        },
+        labels: {
+          style: { cssClass: 'grey--text lighten-2--text fill-color' },
+        },
+      },
+      stroke: {
+        show: true,
+        width: 5,
+        colors: ['transparent'],
+      },
+      tooltip: { theme: 'light' },
+
+      responsive: [
+        {
+          breakpoint: 600,
+          options: {
+            plotOptions: {
+              bar: {
+                borderRadius: 3,
+              },
+            },
+          },
+        },
+      ],
+    }
+  }
+
+  private loadDistributionChart(): void {
+    this.trafficdistributionChart = {
+      series: [12, 8, 15],
+      labels: ['neutral', 'negative', 'positive'],
+      chart: {
+        type: 'donut',
+        fontFamily: "'Plus Jakarta Sans', sans-serif;",
+        foreColor: '#adb0bb',
+        toolbar: {
+          show: false,
+        },
+        height: 160,
+      },
+      colors: ['#f8c076', '#fb977d', '#0085db'],
+      plotOptions: {
+        pie: {
+          donut: {
+            size: '80%',
+            background: 'none',
+            labels: {
+              show: true,
+              name: {
+                show: true,
+                fontSize: '12px',
+                color: undefined,
+                offsetY: 5,
+              },
+              value: {
+                show: true,
+                color: '#98aab4',
+              },
+            },
+          },
+        },
+      },
+      stroke: {
+        show: false,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: true,
+      },
+      responsive: [
+        {
+          breakpoint: 491,
+          options: {
+            chart: {
+              width: 120,
+            },
+          },
+        },
+      ],
+      tooltip: {
+        enabled: false,
+      },
+    };
   }
 }
